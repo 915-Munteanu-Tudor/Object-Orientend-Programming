48f748d892ab0fdb67ca4e60e734f39d22d34684
915-Munteanu-Tudor <72068100+915-Munteanu-Tudor@users.noreply.github.com>
2021-12-12 14:40:52 +0100

    up

--- cpy lb4/cmake-build-debug-coverage/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: T:/materiale/sem2/oop/cpy lb4/cmake-build-debug-coverage/googletest-src/googlemock
-# Build directory: T:/materiale/sem2/oop/cpy lb4/cmake-build-debug-coverage/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
---
+++ cpy lb4/cmake-build-debug/config.tests/static_link_order/CMakeFiles/staticLib.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "T:/Qt/Tools/QtDesignStudio-2.3.0-preview/qt6_design_studio_reduced_version/lib/cmake/Qt6/config.tests/static_link_order/staticlib1.cpp" "T:/materiale/an1/sem2/oop/Object-Orientend-Programming git/cpy lb4/cmake-build-debug/config.tests/static_link_order/CMakeFiles/staticLib.dir/staticlib1.cpp.obj"
+  "T:/Qt/Tools/QtDesignStudio-2.3.0-preview/qt6_design_studio_reduced_version/lib/cmake/Qt6/config.tests/static_link_order/staticlib2.cpp" "T:/materiale/an1/sem2/oop/Object-Orientend-Programming git/cpy lb4/cmake-build-debug/config.tests/static_link_order/CMakeFiles/staticLib.dir/staticlib2.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
